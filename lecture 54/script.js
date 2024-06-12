5a0636c9664f98741427473d1e6f5a3a1cf766b2
itsmuskanmishra <154608418+itsmuskanmishra@users.noreply.github.com>
2024-06-12 19:29:40 +0530

    web dev course

---
+++ lecture 54/script.js
@@ -0,0 +1,10 @@
+alert("Hello world");
+
+console.log("code is running...");
+console.log("code is also running...");
+console.log("code islooking like a wow...");
+
+var a = prompt("Enter your number")
+var isTrue
+
+console.log("your number is" +a)
---
+++ lecture 55/script.js
@@ -0,0 +1,36 @@
+console.log("Hey this tutorial 55")
+
+let a=5; 
+let b=5;
+let c="muskan";
+
+
+console.log(a+b+8)
+console.log(typeof a,typeof b, typeof c)
+
+
+
+let x="muskan";
+let y=22;
+let z=3.55;
+const p=true;
+let q=undefined;
+let r=null;
+
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+
+
+let o={
+    "name":"muskan",
+    "job code": 5600,
+    "is_handsome":true    
+}
+console.log(o);
+o.salary="100crores";
+console.log(o);
+o.salary="500crores";
+console.log(o);
+ 
