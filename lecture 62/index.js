77e6856c055dd5da0807c2d91fefaeba460da87b
itsmuskanmishra <154608418+itsmuskanmishra@users.noreply.github.com>
2024-07-27 08:36:07 +0530

    web dev course

---
+++ lecture 62/index.js
@@ -0,0 +1,2 @@
+// create a business name generator by combinining list of adjectives and shop name and another word
+
---
+++ lecture 63/index.js
@@ -0,0 +1,24 @@
+// let arr =[1,2,3,4,7]
+
+// arr[0]=5666;
+// console.log(arr);
+// console.log(arr.length)
+
+// console.log(arr[0])
+// console.log(arr[2])
+// console.log(arr[4])
+
+// console.log(arr.toString())
+// console.log(arr.toString())
+
+
+console.log(arr.toString())
+console.log(arr.join(" and "))
+
+
+
+// let numbers = [1, 2, 3, 4, 5] 
+// numbers.splice(1, 2)    
+// numbers.splice(1, 3)  
+// numbers.splice(1, 3, 222, 333) 
+// (4) [1, 222, 333, 5]
